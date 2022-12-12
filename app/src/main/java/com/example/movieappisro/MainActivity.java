d14456ccfdbc4ee6daf712ce62563d496b196794
israel2588 <isro_p@hotmail.com>
2022-12-12 14:48:05 -0500

    se sube el proyecto no zipeado

---
+++ app/src/main/java/com/example/movieappisro/MainActivity.java
@@ -0,0 +1,145 @@
+package com.example.movieappisro;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.example.movieappisro.adapters.MovieAdapter;
+import com.example.movieappisro.databinding.ActivityMainBinding;
+import com.example.movieappisro.model.Movie;
+import com.example.movieappisro.viewmodel.MainActivityViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Movie> movies;
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private MainActivityViewModel mainActivityViewModel;
+    private ActivityMainBinding activityMainBinding;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      /*  try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        //setTheme(R.style.SplashIni);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Movie App Gonet");
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+
+        getPopularMovies();
+
+        swipeRefreshLayout = activityMainBinding.swipeLayout;
+        swipeRefreshLayout.setColorSchemeResources(R.color.teal_200);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getPopularMovies();
+            }
+        });
+
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        LinearLayoutManager layoutManager2
+                = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        LinearLayoutManager layoutManagerRM
+                = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        RecyclerView myList = (RecyclerView) findViewById(R.id.rvMovies);
+        RecyclerView myList2 = (RecyclerView) findViewById(R.id.rvMoviesTR);
+        RecyclerView myListRM = (RecyclerView) findViewById(R.id.rvMoviesRP);
+        myList.setLayoutManager(layoutManager);
+        myList2.setLayoutManager(layoutManager2);
+        myListRM.setLayoutManager(layoutManagerRM);
+    }
+
+   private void getPopularMovies() {
+        mainActivityViewModel.getAllMovies().observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> moviesFromLiveData) {
+                movies = (ArrayList<Movie>) moviesFromLiveData;
+                ShowOnRecyclerView();
+                ShowOnRecyclerViewRT();
+                ShowOnRecyclerViewRP();
+            }
+        });
+
+
+    }
+
+    private void ShowOnRecyclerView() {
+
+        recyclerView = activityMainBinding.rvMovies;
+        movieAdapter = new MovieAdapter(this, movies);
+
+        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 1,GridLayoutManager.HORIZONTAL,false));
+        }else{
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        }
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(movieAdapter);
+        movieAdapter.notifyDataSetChanged();
+
+    }
+    private void ShowOnRecyclerViewRT() {
+
+        recyclerView = activityMainBinding.rvMoviesTR;
+        movieAdapter = new MovieAdapter(this, movies);
+
+        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 1,GridLayoutManager.HORIZONTAL,false));
+        }else{
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        }
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(movieAdapter);
+        movieAdapter.notifyDataSetChanged();
+
+    }
+    private void ShowOnRecyclerViewRP() {
+
+        recyclerView = activityMainBinding.rvMoviesRP;
+        movieAdapter = new MovieAdapter(this, movies);
+
+        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 2,GridLayoutManager.HORIZONTAL,false));
+        }else{
+            recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        }
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(movieAdapter);
+        movieAdapter.notifyDataSetChanged();
+
+    }
+}
